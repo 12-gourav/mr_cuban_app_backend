47a750b5632a818b10e3ec96e7124f03f22af262
Gaurav Bajpai <corecomputer143@gmail.com>
2024-09-22 13:42:20 +0530

    Fixed Changes

--- routes/lead_route.js
+++ routes/lead_route.js
@@ -14,7 +14,7 @@ const router = express.Router();
 router.post("/create/lead", CreateLead);
 router.get("/leads", DisplayOrderLeads);
 router.post("/accept/lead/driver", AcceptOrderLead);
-router.get("/accept/lead/customer", AcceptOrderLeadByCustomer);
+router.post("/accept/lead/customer", AcceptOrderLeadByCustomer);
 router.get("/cancel/lead/customer", CancelRideByUser);
 router.get("/get/lead", DisplayCustomerLead);
 router.get("/get/lead/drivers", DisplayRides);
